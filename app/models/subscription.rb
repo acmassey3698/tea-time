c55b7387652ed4929fa80f334f0e07492f82b167
Andrew Massey <acmassey@ncsu.edu>
2022-01-05 09:26:31 -0500

    Set up enum on subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,15 @@
 class Subscription < ApplicationRecord
   belongs_to :customer
   belongs_to :tea
+
+  enum status: {
+    active: 1,
+    cancelled: 2,
+  }
+
+  enum frequency: {
+    weekly: 1,
+    monthly: 2,
+    yearly: 3
+  }
 end
