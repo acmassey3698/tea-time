ae6eedb18cd2d4b4113467aa30fa218881225554
Andrew Massey <acmassey@ncsu.edu>
2022-01-06 21:24:48 -0500

    Fix strings on the other serializer

--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -1,15 +1,15 @@
 class SubscriptionSerializer
   def self.one_sub(subscription)
     {
-      data: {
-        id: subscription.id,
-        type: "subscription",
-        attributes: {
-          customer_email: subscription.customer.email,
-          tea_name: subscription.tea.name,
-          price: subscription.price,
-          status: subscription.status,
-          frequency: subscription.frequency
+      'data': {
+        'id': subscription.id,
+        'type': "subscription",
+        'attributes': {
+          'customer_email': subscription.customer.email,
+          'tea_name': subscription.tea.name,
+          'price': subscription.price,
+          'status': subscription.status,
+          'frequency': subscription.frequency
         }
       }
     }
